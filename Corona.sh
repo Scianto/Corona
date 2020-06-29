331cf7b41f0e17b0abedc8359ff75ac27459ba5f
sci anto <65482441+Scianto@users.noreply.github.com>
2020-06-29 17:13:09 +0530

    Add files via upload

---
+++ Corona.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#Corona v1.1
+#corona live cases updated daily
+#author : sci_anto
+#git clone https://github.com/Scianto/Corona
+
+printf "\e[31m ██████╗ ██████╗ ██████╗  ██████╗ ███╗   ██╗ █████╗             \e[0m\n"
+printf "\e[31m ██╔════╝██╔═══██╗██╔══██╗██╔═══██╗████╗  ██║██╔══██╗    \e[0m\n"
+printf "\e[31m ██║     ██║   ██║██████╔╝██║   ██║██╔██╗ ██║███████║             \e[0m\n"
+printf "\e[31m ██║     ██║   ██║██╔══██╗██║   ██║██║╚██╗██║██╔══██║            \e[0m\n"
+printf "\e[31m ╚██████╗╚██████╔╝██║  ██║╚██████╔╝██║ ╚████║██║  ██║       \e[0m\n"
+printf "\e[31m   ╚═════╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝             \e[0m\n"
+printf "\n"
+printf "                        \e[1;37;1;41m  Script  by  Sci_anto  \e[0m\n" 
+printf "
+printf " this tool is not a live counter, it will update daily at 00:00 \n"
+printf "\n"
+printf "         \e[1;37;1;41m CORONA CASES \e[0m\n"
+printf "         \e[32m         10,265,207   \e[0m\n"
+printf "\n"
+printf "         \e[1;37;1;41m   DEATHS    \e[0m\n"
+printf "         \e[32m          504,744   \e[0m\n"
+printf "\n"
+printf "         \e[1;37;1;41m   RECOVERED  \e[0m\n"
+printf "         \e[32m          5,566,616  \e[0m\n"
+
