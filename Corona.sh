970ca08acb0c1ae199fe9ab6b3785b20804ba284
sci anto <65482441+Scianto@users.noreply.github.com>
2020-06-29 17:14:29 +0530

    Update Corona.sh

--- Corona.sh
+++ Corona.sh
@@ -4,16 +4,16 @@
 #author : sci_anto
 #git clone https://github.com/Scianto/Corona
 
-printf "\e[31m ██████╗ ██████╗ ██████╗  ██████╗ ███╗   ██╗ █████╗             \e[0m\n"
+printf "\e[31m  ██████╗ ██████╗ ██████╗  ██████╗ ███╗   ██╗ █████╗             \e[0m\n"
 printf "\e[31m ██╔════╝██╔═══██╗██╔══██╗██╔═══██╗████╗  ██║██╔══██╗    \e[0m\n"
 printf "\e[31m ██║     ██║   ██║██████╔╝██║   ██║██╔██╗ ██║███████║             \e[0m\n"
 printf "\e[31m ██║     ██║   ██║██╔══██╗██║   ██║██║╚██╗██║██╔══██║            \e[0m\n"
 printf "\e[31m ╚██████╗╚██████╔╝██║  ██║╚██████╔╝██║ ╚████║██║  ██║       \e[0m\n"
 printf "\e[31m   ╚═════╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝             \e[0m\n"
 printf "\n"
-printf "                        \e[1;37;1;41m  Script  by  Sci_anto  \e[0m\n" 
+printf "          \e[1;37;1;41m  Script  by  Sci_anto  \e[0m\n" 
 printf "
-printf " this tool is not a live counter, it will update daily at 00:00 \n"
+printf "\e[32m this tool is not a live counter, it will update daily at 00:00 \e[0m\n"
 printf "\n"
 printf "         \e[1;37;1;41m CORONA CASES \e[0m\n"
 printf "         \e[32m         10,265,207   \e[0m\n"
